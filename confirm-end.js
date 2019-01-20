a366c0d7750715f16d2bdb7eb60ae0fb5e675bef
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-20 14:57:03 +0200

    improved solution

--- confirm-end.js
+++ confirm-end.js
@@ -2,8 +2,7 @@
 function confirmEnding(str, target) {
   // "Never give up and good luck will find you."
   // -- Falcor
-  if (str.substr(str.length - 1) === target.length) return true;
-  return false;
+  return str.substr(str.length - target.length) === target;
 }
 
 console.log(confirmEnding("Bastian", "n"));
