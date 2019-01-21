6480f2cd8c5f2e7554228e1ccd6048aa970526f9
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-21 11:52:47 +0200

    truncate task done

---
+++ truncate.js
@@ -0,0 +1,11 @@
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + '...';
+  }
+  return str;
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
