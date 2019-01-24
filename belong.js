ab28850e6dad6da0c050bf1aa292cc7b3dd36b99
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-24 12:00:27 +0200

    advanced solution

--- belong.js
+++ belong.js
@@ -3,8 +3,18 @@
 
 function getIndexToIns(arr, num) {
   arr = [...arr, num];
-  arr.sort((a, b) => a - b);
-  return arr.indexOf(num);
+  arr.sort((a, b) => a - b); // OR: arr.sort((a, b) => a > b);
+  return arr.indexOf(num); // OR: return arr.findIndex(v => v === num);
 }
 
 console.log(getIndexToIns([40, 60], 50));
+
+// Given advanced solution
+function getIndexToIns2(arr, num) {
+  return arr
+    .concat(num)
+    .sort((curr, next) => curr > next)
+    .indexOf(num);
+}
+
+console.log(getIndexToIns2([10, 20, 30, 40, 50], 30));
