14b412d40814b04aa8632375c4c201b17362a007
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-24 11:32:18 +0200

    index task done

--- belong.js
+++ belong.js
@@ -1,2 +1,10 @@
 
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
 
+function getIndexToIns(arr, num) {
+  arr = [...arr, num];
+  arr.sort((a, b) => a - b);
+  return arr.indexOf(num);
+}
+
+console.log(getIndexToIns([40, 60], 50));
