80d667d2f81570fef32181eb70bd0461b6cda4bf
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-26 23:33:35 +0200

    intro

---
+++ FUNCTIONAL/about.js
@@ -0,0 +1,5 @@
+
+// Functional programming is a style of programming where solutions are simple, isolated functions, without any side effects outside of the function scope.
+
+// INPUT -> PROCESS -> OUTPUT
+
