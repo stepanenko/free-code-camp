8adc827a4bcd8157f459576733b8b04af7d72a35
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-26 23:38:39 +0200

    first task done

--- FUNCTIONAL/about.js
+++
@@ -1,5 +0,0 @@
-
-// Functional programming is a style of programming where solutions are simple, isolated functions, without any side effects outside of the function scope.
-
-// INPUT -> PROCESS -> OUTPUT
-
---
+++ FUNCTIONAL/intro.js
@@ -0,0 +1,23 @@
+
+// Functional programming is a style of programming where solutions are simple, isolated functions, without any side effects outside of the function scope.
+
+// INPUT -> PROCESS -> OUTPUT
+
+// TASK: Call the getTea function to get 40 cups of tea for the team, and store them in the tea4TeamFCC variable.
+
+const prepareTea = () => 'greenTea';
+
+const getTea = (numOfCups) => {
+  const teaCups = [];
+  
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+
+  return teaCups;
+};
+
+const tea4TeamFCC = getTea(40);
+
+console.log(tea4TeamFCC);
