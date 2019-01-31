9f94de8a8df626e81d21d8c6b7f05d766af6f5ed
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-31 11:00:14 +0200

    last task in progress

---
+++ BASIC ALG/chunks.js
@@ -0,0 +1,12 @@
+
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  
+  return arr;
+}
+
+// chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+let arr = ["a", "b", "c", "d"];
+
