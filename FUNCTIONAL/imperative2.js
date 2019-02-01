b8844da04342b3328541edc95d3ff4311f7edcb8
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-02-01 12:57:18 +0200

    task done

--- FUNCTIONAL/imperative2.js
+++ FUNCTIONAL/imperative2.js
@@ -3,3 +3,14 @@
 
 // Recall that in functional programming, changing or altering things is called mutation, and the outcome is called a side effect. A function, ideally, should be a pure function, meaning that it does not cause any side effects.
 
+// TASK: Fill in the code for the function incrementer so it returns the value of the global variable fixedValue increased by one.
+
+// the global variable
+var fixedValue = 4;
+
+function incrementer () {
+  return fixedValue + 1;
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
