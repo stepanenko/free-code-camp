3e246fcfbd8cf332a4689c34e2e8e9a920a96881
Serhii Stepanenko <sstepane@macmini5478.ddns.softservecom.com>
2019-02-20 12:50:33 +0200

    my solution  done

--- INTER ALG/wherefore.js
+++ INTER ALG/wherefore.js
@@ -2,13 +2,20 @@
 // TASK: Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
 
 function whatIsInAName(collection, source) {
-  // What's in a name?
   var arr = [];
-  // Only change code below this line
-  
-  
-  // Only change code above this line
+  for (let i = 0; i < collection.length; i++) {
+    var count = 0;
+    for (let el in source) {
+      if (collection[i][el] === source[el]) {
+        count++;
+      }
+    }
+    if (count === Object.keys(source).length) {
+      arr.push(collection[i])
+    }
+  }
+  console.log(arr);
   return arr;
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
