f4b15a1103ef5f15bce1fc5b8995f47d82d4eccb
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-02-23 14:26:01 +0200

    new task

---
+++ INTER ALG/spinal.js
@@ -0,0 +1,9 @@
+
+// TASK: Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  
+  return str;
+}
+
+spinalCase('This Is Spinal Tap');
