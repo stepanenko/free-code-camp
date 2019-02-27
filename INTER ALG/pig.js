f36bbd4e1a9e60930be6e8235d7ff0b91847da20
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-02-27 22:10:52 +0200

    started new solution

--- INTER ALG/pig.js
+++ INTER ALG/pig.js
@@ -1,7 +1,7 @@
 
 // TASK: take the first consonant of a word, move it to the end and suffixe an "ay". If a word begins with a vowel you just add "way" to the end.
 
-// My solution:
+// My solution 1:
 function translatePigLatin(str) {
   var index = str.search(/a|e|o|i|u/);
 
@@ -11,5 +11,8 @@ function translatePigLatin(str) {
     return str + 'way';
   } else return str + 'ay';
 }
+
+// My solution 2 with a hint 'substr()'
+
 
 console.log(translatePigLatin("glove"));
