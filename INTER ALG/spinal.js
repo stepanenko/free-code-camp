237aada4f4caae7cc1ae8d77f8900254b7abece5
Serhii Stepanenko <sstepane@macmini5478.ddns.softservecom.com>
2019-02-27 12:52:13 +0200

    all done

--- INTER ALG/spinal.js
+++ INTER ALG/spinal.js
@@ -12,12 +12,23 @@ function spinalCase(str) {
 function spinalCase1(str) {
   // Create a variable for the white space and underscores.
   var regex = /\s+|_+/g;
-
   // Replace low-upper case to low-space-uppercase
   str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
-
   // Replace space and underscore with '-'
   return str.replace(regex, '-').toLowerCase();
 }
 
-console.log(spinalCase('This Is SpinalTap'));
+// Given solution 2:
+function spinalCase2(str) {
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  // Split on whitespace and underscores and join with dash
+  return str.toLowerCase().split(/(?:_| )+/).join('-');
+}
+
+// Given solution 3:
+function spinalCase3(str) {
+  return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase();
+}
+
+console.log(spinalCase3('This Is SpinalTap'));
