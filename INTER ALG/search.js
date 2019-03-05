e1d48d82f3ff7f157430cba9ba05e537372310d4
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2019-03-05 11:51:10 +0200

    Create search.js

---
+++ INTER ALG/search.js
@@ -0,0 +1,5 @@
+
+// TASK: Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+// First argument is the sentence to perform the search and replace on.
+// Second argument is the word that you will be replacing (before).
+// Third argument is what you will be replacing the second argument with (after).
