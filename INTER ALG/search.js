8d0dd39039dc07919ebea8b6503d3a9970d69bbe
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2019-03-05 11:52:36 +0200

    Update search.js

--- INTER ALG/search.js
+++ INTER ALG/search.js
@@ -3,3 +3,12 @@
 // First argument is the sentence to perform the search and replace on.
 // Second argument is the word that you will be replacing (before).
 // Third argument is what you will be replacing the second argument with (after).
+
+function myReplace(str, before, after) {
+  return str;
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+// Note: Preserve the case of the first character in the original word when you are replacing it.
+// For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
