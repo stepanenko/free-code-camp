728c404560d7277a817b66a99fb5bf662b48b694
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-05-05 15:02:40 +0300

    start

---
+++ INTER ALG/pair.js
@@ -0,0 +1,8 @@
+
+// TASK: The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array. Base pairs are a pair of AT and CG. Match the missing element to the provided character. Return the provided character as the first element in each array.
+
+function pairElement(str) {
+  return str;
+}
+
+pairElement("GCG");
