5126cbcd80f62f15c843df794f62ea23d438a598
Serhiy Stepanenko <41634824+stepanenko@users.noreply.github.com>
2019-05-20 01:04:02 +0300

    Update search.js

--- INTER ALG/search.js
+++ INTER ALG/search.js
@@ -4,6 +4,7 @@
 // Second argument is the word that you will be replacing (before).
 // Third argument is what you will be replacing the second argument with (after).
 
+
 function myReplace(str, before, after) {
   if (before[0] === before[0].toUpperCase()) {
     after = after[0].toUpperCase() + after.slice(1);
