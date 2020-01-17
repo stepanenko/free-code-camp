f1a6c048b5977ca6e89d3dc9789a9f14f1a92955
stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-17 13:31:42 +0200

    done

---
+++ BASIC ALG/recurse3.js
@@ -0,0 +1,27 @@
+
+// TASK: The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter.
+
+// My solution:
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum == endNum) return [startNum]; // or if(startNum - endNum === 0) ...
+  const arr = rangeOfNumbers(startNum, endNum - 1);
+  arr.push(endNum);
+  return arr;
+};
+
+// Solution 2:
+function rangeOfNumbers2(startNum, endNum) {
+  return startNum == endNum 
+    ? [startNum] 
+    : rangeOfNumbers2(startNum, endNum - 1).concat(endNum);
+};
+
+// Solution 3:
+function rangeOfNumbers3(startNum, endNum) {
+  return startNum == endNum 
+    ? [startNum] 
+    : [...rangeOfNumbers3(startNum, endNum - 1), endNum];
+};
+
+const output = rangeOfNumbers3(2, 6);
+console.log(output);
