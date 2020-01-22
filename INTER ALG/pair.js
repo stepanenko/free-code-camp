471683748a7c64e0e0818ca0a18ce4c2e855583f
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-22 21:01:08 +0200

    pair DNA task done

--- INTER ALG/pair.js
+++ INTER ALG/pair.js
@@ -1,8 +1,16 @@
 
-// TASK: The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array. Base pairs are a pair of AT and CG. Match the missing element to the provided character. Return the provided character as the first element in each array.
+// TASK: Take each character, get its pair, and return the results as a 2d array. Base pairs are a pair of AT and CG. Match the missing element to the provided character. Return the provided character as the first element in each array.
 
+// My solution:
 function pairElement(str) {
-  return str;
+  return str.split('').map(e => {
+    if (e === 'A') return [e, 'T'];
+    if (e === 'T') return [e, 'A'];
+    if (e === 'C') return [e, 'G'];
+    if (e === 'G') return [e, 'C'];
+  });
 }
 
-pairElement("GCG");
+
+let result = pairElement("ATCGA");
+console.log(result);
