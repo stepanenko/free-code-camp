bb520074b0fbd2eccadfa672f9f3f05c81395ca1
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-25 10:20:24 +0200

    my new solutions

--- INTER ALG/pair.js
+++ INTER ALG/pair.js
@@ -1,7 +1,7 @@
 
 // TASK: Take each character, get its pair, and return the results as a 2d array. Base pairs are a pair of AT and CG. Match the missing element to the provided character. Return the provided character as the first element in each array.
 
-// My solution:
+// My solution 1:
 function pairElement(str) {
   return str.split('').map(e => {
     if (e === 'A') return [e, 'T'];
@@ -28,5 +28,34 @@ function pairElement2(str) {
   return newArr;
 }
 
-let result = pairElement2("ATCGA");
+// My solution 3:
+function pairElement3(str) {
+  let newArr = [];
+  for (let e of str) {
+    if (e === 'A') {
+      newArr = [...newArr, [e, 'T']];
+    } else if (e === 'T') {
+      newArr = [...newArr, [e, 'A']];
+    } else if (e === 'C') {
+      newArr = [...newArr, [e, 'G']];
+    } else {
+      newArr = [...newArr, [e, 'C']];
+    }
+  }
+  return newArr;
+}
+
+// My solution 4:
+function pairElement4(str) {
+  let newArr = [];
+  for (let e of str) {
+    newArr = e === 'A' ? [...newArr, [e, 'T']] :
+             e === 'T' ? [...newArr, [e, 'A']] :
+             e === 'C' ? [...newArr, [e, 'G']] :
+                         [...newArr, [e, 'C']];
+  }
+  return newArr;
+}
+
+let result = pairElement4("ATCGA");
 console.log(result);
