0b848209feba2ca1c369f66c54312af71cf241fa
stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-30 17:18:35 +0200

    done

--- INTER ALG/letters.js
+++ INTER ALG/letters.js
@@ -1,19 +1,18 @@
 
 // TASK: Find the missing letter in the passed letter range and return it. If all letters are present in the range, return undefined.
 
-// My Solution:
+// My Solution 1:
 function fearNotLetter(str) {
   for (let i = 0; i < str.length - 1; i++) {
     if (str.charCodeAt(i) !== (str.charCodeAt(i + 1) - 1)) {
       return String.fromCharCode(str.charCodeAt(i) + 1);
     }
   }
-  return undefined;
 }
 
 
-let output = fearNotLetter("abcdefghijklmnopqrstuvwxyz");
+let output = fearNotLetter("abcdeg");
 console.log(output);
 
 
-let str = 'abcdefghijklmnopqrstuvwxyz';
+// let str = 'abcdefghijklmnopqrstuvwxyz';
