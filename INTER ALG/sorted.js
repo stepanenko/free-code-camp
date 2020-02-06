1ef200292d8fc18a3328692d3f965f555cf3dc0e
stepanenko <serhiy.stepanenko@aiesec.net>
2020-02-06 12:25:36 +0200

    done

--- INTER ALG/sorted.js
+++ INTER ALG/sorted.js
@@ -1,20 +1,27 @@
 
 // TASK: Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
 
-// My solution:
-function uniteUnique(arr) {
-  let uArray = [];
-  [...arguments].forEach((array, index) => {
-    uArray.push(...array);
-  });
-  let resultArray = [uArray[0]];
-  for (let i = 1; i < uArray.length; i++) {
-    if (!resultArray.includes(uArray[i])) {
-      resultArray.push(uArray[i]);
+// My solution 1:
+function uniteUnique() {
+  let arr = [];
+  [...arguments].forEach(array => arr.push(...array));
+  let resultArray = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (!resultArray.includes(arr[i])) {
+      resultArray.push(arr[i]);
     }
   }
   return resultArray;
 }
 
-let output = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+// My solution 2:
+function uniteUnique2() {
+  let arr = [];
+  [...arguments].forEach(array => arr.push(...array));
+  let resultArray = [arr[0]];
+  arr.forEach(e => !resultArray.includes(e) ? resultArray.push(e) : null);
+  return resultArray;
+}
+
+let output = uniteUnique2([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 console.log(output);
