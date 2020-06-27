352fae5819c7b5d696131b1115f8055ad110d641
stepanenko <serhiy.stepanenko@aiesec.net>
2020-06-27 22:03:49 +0300

    my solutions added

--- INTERM ALGOR/drop.js
+++ INTERM ALGOR/drop.js
@@ -1,5 +1,6 @@
 
-// TASK: Given the array, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+// TASK: Given the array, iterate through and remove each element starting from the first element (the 0 index)
+// until the function func returns true when the iterated element is passed through it.
 
 // Then return the rest of the array once the condition is satisfied, otherwise - empty array.
 
@@ -15,20 +16,20 @@ function myDropElements1(arr, func) {
 }
 
 // My solution 2 (shortened 1):
-function myDropElements2(arr, func) {   // 3.7ms
+function myDropElements2(arr, func) {   // 3.4ms
   let res = [];
   arr.forEach((el, i) => func(el) ? res = res.concat(i) : null);
   return res.length ? arr.slice(res[0]) : [];
 }
 
 // My solution 3:
-function myDropElements3(arr, func) {   // 3.7ms
+function myDropElements3(arr, func) {   // 3.4ms
   let index = arr.indexOf(arr.filter(func)[0]);
   return index !== -1 ? arr.slice(index) : [];
 }
 
 console.time('3');
-console.log(myDropElements3([1, 2, 3, 4], function (n) { return n >= 3; }));  // should return [3, 4]
+console.log(myDropElements2([1, 2, 3, 4], function (n) { return n >= 3; }));  // should return [3, 4]
 console.log(myDropElements3([0, 1, 0, 1], function (n) { return n === 1; }));  // should return [1, 0, 1]
 console.log(myDropElements3([1, 2, 3], function (n) { return n > 0; }));  // should return [1, 2, 3]
 console.log(myDropElements3([1, 2, 3, 4], function (n) { return n > 5; }));  // should return []
